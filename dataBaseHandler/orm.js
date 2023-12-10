d8c2051d19d7bdc68962f146caf248601e148fd0
Hesam Behboudi <he.behboudi@mohaymen.ir>
2023-12-10 14:36:29 +0330

    prisma initialaztion

--- dataBaseHandler/orm.js
+++ dataBaseHandler/orm.js
@@ -1,10 +1,18 @@
-
+const { PrismaClient } = require("@prisma/client")
+const prisma=new PrismaClient();
 class OrmHandler{
     constructor(){
         if(!OrmHandler.instance){
             OrmHandler.instance=this
         }
         return OrmHandler.instance
+    }
+   async getUser(email,password){
+        const user= await prisma.user.findFirst({
+            where:{email , password}
+        })
+
+        return user;
     }
 }
 
---
+++ queries/user.pgsql
@@ -0,0 +1,12 @@
+TRUNCATE "User";
+DELETE FROM "User";
+
+SELECT * from "User"
+
+
+INSERT Into "User"
+    (id ,email,name,"userName",password)
+    VALUES 
+    (1,'hes@gmail.com','hes','hes',1234);
+
+
