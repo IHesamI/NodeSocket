298e24000ab6a85fa73cb7bf6a4ce63a167e6b54
hesam <bhesam2000@yahoo.com>
2023-12-15 21:09:51 +0330

    add write and read stream and send the chunks to client

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const fs=require('fs');
+
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { Buffer} = require('node:buffer');
 // const login=require('./api/login');
 const { Server } = require("socket.io");
 // const { default: msgHandler } = require("./api/message");
@@ -22,11 +25,35 @@ app.get("/", (req, res) => {
 
 app.post("/msg", upload.single("chunk"), (req, res) => {
   try {
-    console.error("zarp", req.file);
+    // const image =buffer.from(req.file.buffer);
+    console.error();
+    const fsize=req.file.size
+    const buf=req.file.buffer;
+    const ws=fs.createWriteStream('img.bin',function(err){})
+    ws.write(buf);
+    ws.end();
     res.send({ status: "200" });
   } catch (e) {
-    res.send({ status: "403" });
+    res.send({ status: "403" ,error:'zarp'});
   }
 });
+const CHUNK_SIZE= 1024;
+app.get('/getmsg',async (req,res)=>{
+  const imageBuffer= await fs.readFileSync('./img.bin');
+  console.error(imageBuffer.length);
+  const size=imageBuffer.length;
+  let offset=0;
+  while(offset<size){
+    const chunk = imageBuffer.slice(offset, offset + CHUNK_SIZE);
+    res.write(chunk);
+    // res.write('data: ping');
+
+    offset += CHUNK_SIZE;
+  }
+
+  res.end();
+
+  // res.send({status:303,buffer:imageBuffer});
+})
 
 module.exports = app;
